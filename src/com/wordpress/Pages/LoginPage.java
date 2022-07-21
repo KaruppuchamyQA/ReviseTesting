d3b423c6139524984a2ebded90fce295db6b9e13
karuppuchamy <karuppuchamy692@gmail.com>
2022-07-21 14:17:44 +0530

    first commit

---
+++ src/com/wordpress/Pages/LoginPage.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wordpress.Pages;
+
+/**
+ * @author KaruppuchamyKalimuthu
+ * This class will store all the locator and methods of login page
+ *
+ */
+public class LoginPage 
+{
+
+	By us=By.id("usernameOrEmail");	
+	
+	
+	
+}
